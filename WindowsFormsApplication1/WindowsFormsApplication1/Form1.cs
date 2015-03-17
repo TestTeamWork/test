02454fe4ae0e1f1e6dfb7968f5a78ddab7a4d681
yancey <yancey1204@gmail.com>
2015-03-17 23:02:13 +0100

    add a button

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -17,5 +17,10 @@ namespace WindowsFormsApplication1
             InitializeComponent();
             int a = 0;
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
