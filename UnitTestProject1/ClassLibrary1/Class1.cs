b4adc84dedd09d755116cf106b14c33d3b66e303
Haiming <haiming12322@gmail.com>
2015-03-25 16:53:19 -0700

    TEST

--- UnitTestProject1/ClassLibrary1/Class1.cs
+++ UnitTestProject1/ClassLibrary1/Class1.cs
@@ -8,5 +8,13 @@ namespace ClassLibrary1
 {
     public class Class1
     {
+        public bool IsPositive(double value)
+        {
+            if (value > 0)
+                return true;
+            return false;
+        }
+
+
     }
 }
